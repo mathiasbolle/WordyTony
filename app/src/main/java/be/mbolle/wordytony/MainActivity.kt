8b4ba87976b1b5138b1c4da244f99e30a85e88a8
Mathias Bolle <mbolle2310@gmail.com>
2025-02-16 19:14:04 +0100

    Main screen implementation

---
+++ app/src/main/java/be/mbolle/wordytony/MainActivity.kt
@@ -0,0 +1,74 @@
+package be.mbolle.wordytony
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import be.mbolle.wordytony.ui.HomeScreen
+import be.mbolle.wordytony.ui.Offset
+import be.mbolle.wordytony.ui.WordyTonyButton
+import be.mbolle.wordytony.ui.theme.WordyTonyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WordyTonyTheme {
+                App()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun App() {
+    WordyTonyTheme {
+        Scaffold(modifier =
+            Modifier
+                .fillMaxSize()
+                .safeDrawingPadding()
+                .padding(horizontal = 25.dp),
+            topBar = { WordyTonyTopAppBar(title = stringResource(R.string.title), modifier = Modifier.fillMaxWidth())}
+            ) { innerPadding ->
+            HomeScreen(modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding))
+        }
+    }
+}
+
+@Composable
+fun WordyTonyTopAppBar(modifier: Modifier = Modifier, title: String) {
+    Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = modifier,) {
+        Column {
+            Text(text = title, style = MaterialTheme.typography.titleLarge)
+            Text(stringResource(R.string.subtitle), style = MaterialTheme.typography.titleMedium)
+        }
+        WordyTonyButton(onClick = {}, offset = Offset(4.dp, 4.dp), shape = CircleShape, modifier = Modifier.height(IntrinsicSize.Min)) {
+            Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings icon",)
+        }
+    }
+}
---
+++ app/src/main/java/be/mbolle/wordytony/ui/theme/Type.kt
@@ -0,0 +1,71 @@
+package be.mbolle.wordytony.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import be.mbolle.wordytony.R
+
+
+private val pushPinsBold = FontFamily(
+    Font(R.font.pushpins_light),
+    Font(R.font.pushpins_bold)
+)
+
+private val pushPinsLight = FontFamily(
+    Font(R.font.pushpins_light)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = pushPinsBold,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 30.sp,
+        letterSpacing = 0.sp,
+        lineHeight = 0.sp
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = pushPinsLight,
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = pushPinsLight,
+        fontWeight = FontWeight.Normal,
+        fontSize = 25.sp,
+        letterSpacing = 0.sp
+    )
+
+    /* Other default text styles to override
+
+
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
